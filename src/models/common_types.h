c75071386d05dc9f6ebbe6573bf223eab23ef845
Daniel Todorov <danieltod2003@gmail.com>
2026-04-19 21:11:39 +0300

    add Coordinate::distance_to helper
    Used in the upcoming allocate_greedy migration to replace inline
    sqrt(dx*dx+dy*dy) arithmetic.

--- src/models/common_types.h
+++ src/models/common_types.h
@@ -1,10 +1,17 @@
 #pragma once
 
+#include <cmath>
 #include <string>
 
 struct Coordinate {
     double x;
     double y;
+
+    double distance_to(const Coordinate& other) const {
+        double dx = x - other.x;
+        double dy = y - other.y;
+        return std::sqrt(dx * dx + dy * dy);
+    }
 };
 
 struct SpotData {
